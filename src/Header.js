b872087700296b90425bfd9b1d8e517098ea9403
Ilya Chernov <ilya.v.chernov@gmail.com>
2017-06-28 14:53:56 +0300

    convert Header to stateless functional component

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return <h1>Awesome List</h1>;
-  }
+function Header() {
+  return <h1>Awesome List</h1>;
 }
 
 export default Header;
